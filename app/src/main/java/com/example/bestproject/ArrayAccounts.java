989cffaf5a994fee7932c4e2d444026169ee8abc
Yardra <kavkazs777@gmail.com>
2023-05-17 00:23:08 +0200

    billie jean

---
+++ app/src/main/java/com/example/bestproject/ArrayAccounts.java
@@ -0,0 +1,9 @@
+package com.example.bestproject;
+
+public class ArrayAccounts {
+
+String[] nickArray = new String[] {"Admin"};
+String[] phoneNumArray = new String[] {"000"};
+String[] passwordArray = new String[] {"000"};
+
+}
---
+++ app/src/main/java/com/example/bestproject/SecondActivty.java
@@ -0,0 +1,14 @@
+package com.example.bestproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen2);
+    }
+}
